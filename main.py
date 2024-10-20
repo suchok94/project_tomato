cb0bced5f8ca5ec2247e178ac48933d5512d5693
suchok94 <suchok94@gmail.com>
2024-10-20 14:01:53 +0300

    фича. добавил кткинтер, переписал entry point в main

---
+++ main.py
@@ -0,0 +1,21 @@
+from modules.app import App
+import customtkinter
+
+
+def main():
+
+    root = customtkinter.CTk()
+    app = App(root)
+    app.root.geometry("600x400")
+    app.root.mainloop()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
