bd3ba46c1d167de6a6a78dcc384e7d61d73d66c1
suchok94 <suchok94@gmail.com>
2024-11-01 16:50:31 +0300

    модифай, переделал пути на файл storage_low_buisness_logic в методах класса serviceStorage

--- modules/storage.py
+++ modules/storage.py
@@ -12,12 +12,12 @@ class ServiceStorage:
     def __init__(self):
         # self.__storage = Storage()
         self.__expansion = 'json'
-        self.__path = storage_lbl.take_path()
+        self.__path = storage_lbl.take_path(self.__expansion)
 
         if os.path.isfile(self.__path):
-            self.__storage = self.load(self.__path, 'json')
+            self.__storage = self.load()
         else:
-            self.__storage = ServiceStorage.create(self, self.__path)
+            self.__storage = ServiceStorage.create(self)
 
 
 
@@ -26,26 +26,30 @@ class ServiceStorage:
         return self.__storage
 
 
-    def create(self, path):
+    def create(self):
         '''
         создаёт новое хранилище, создавая объект класса Storage и сохраняя его в json файл
         :return:
         '''
         storage = Storage()
-        with open(path, "w") as write_file:
-            json.dump(storage.toJson(), write_file)
+        if self.__expansion == 'json':
+            storage_lbl.json_save(storage, self.__path)
+
+        elif self.__expansion == 'txt':
+            storage_lbl.txt_save(storage, self.__path)
+
         return storage
 
 
-    def save(self, storage, path, expansion):
+    def save(self, storage, expansion):
         '''
         сохраняет данные из 'storage' в файл с 'expansion'
         :param self:
         :param expansion:
         :return:
         '''
-        if expansion == 'json':
-            with open(path, "w") as write_file:
+        if self.__expansion == 'json':
+            with open(self.__path, "w") as write_file:
                 json.dump(storage.toJson(), write_file)
 
 
@@ -58,7 +62,13 @@ class ServiceStorage:
         return data
 
 
+    @property
+    def expansion(self):
+        return self.__expansion
 
+    @expansion.setter
+    def expansion(self, expansion):
+        self.__expansion = expansion
 
 
 class Storage:
@@ -75,3 +85,6 @@ class Storage:
 
     def toJson(self):
         return json.dumps(self, default=lambda x: x.__dict__, sort_keys=True, indent=4)
+
+    def toTxt(self):
+        return 'заглушка'
