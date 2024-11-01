04fbf5c83ac734b07efb47797e7928ac0969ba62
suchok94 <suchok94@gmail.com>
2024-11-01 16:31:54 +0300

    фича, перенёс метод take_path из storage.py

--- modules/storage_low_buisness_logic.py
+++ modules/storage_low_buisness_logic.py
@@ -1,4 +1,5 @@
 import json
+import configparser
 
 def json_load(path):
     with open(path, "r") as read_file:
@@ -9,3 +10,11 @@ def txt_load(path):
     with open(path, "r") as read_file:
         data = read_file.read()
     return data
+
+def take_path():
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    path = config.get('Paths', 'storage_path')
+
+    return path
